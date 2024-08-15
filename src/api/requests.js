fb1fe85b380ab6f63c42eca4af8ebe56127ff886
Hizeros <3290606026@qq.com>
2024-08-16 06:37:42 +0800

    fix: error in request

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,20 +1,29 @@
-// import axios from 'axios';
-//
-// // 创建axios实例并配置基础URL
-// // export const apiClient = axios.create({
-// //   baseURL: 'https://oasis-backend.zeabur.app/',
-// // });
-//
-// export const apiClient = axios.create({
-//   baseURL: 'http://127.0.0.1:5000/',
-// });
-
 import Taro from '@tarojs/taro';
 
-// const BASE_URL = 'https://oasis-backend.zeabur.app/';
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = 'https://oasis-backend.zeabur.app';
+// const BASE_URL = "http://localhost:5000"
 
 export const apiClient = {
-  get: (url, data) => {return Taro.request({url:`${BASE_URL}${url}`, data, method:'GET'})},
-  post: (url, data) => {return Taro.request({url:`${BASE_URL}${url}`, data, method:'POST'})}
+  get: (url, data) => {
+    return Taro.request({
+      url: `${BASE_URL}${url}`,
+      data,
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    });
+  },
+  post: (url, data) => {
+    return Taro.request({
+      url: `${BASE_URL}${url}`,
+      data,
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    });
+  }
 }
